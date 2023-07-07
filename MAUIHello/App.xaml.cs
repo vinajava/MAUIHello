6a1f6cb5887bbb2a69fd29047ba54c435ca06986
Lam Vu <>
2023-07-07 10:44:12 +1000

    Init

---
+++ MAUIHello/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIHello;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MAUIHello/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUIHello;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
